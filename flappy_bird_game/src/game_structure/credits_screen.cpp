4aa404fbc12efeb2b3cff0af1ea1f3dcdbafe364
MrTomas <aquista35@gmail.com>
2020-02-05 19:16:07 -0300

    creditos ajustados

--- flappy_bird_game/src/game_structure/credits_screen.cpp
+++ flappy_bird_game/src/game_structure/credits_screen.cpp
@@ -45,7 +45,7 @@ namespace Flappy_Bird
 			DrawText("Credits", GetScreenWidth() / 2 - 100, GetScreenHeight() / 5, fontTittle, GREEN);
 
 			DrawRectangle(GetScreenWidth() / 2 - 175, GetScreenHeight() / 2 - 100, 350, 300, GRAY);
-			DrawText("Main programer: Tomas Aquistapace\nMain artist: Joaquin\n\nMusic:\n Halloween Fun - by pinkzebra\n We all gonna die - by Loyalty_Freak_Music", (GetScreenWidth() / 2 - 175) + (350 / 2) - (MeasureText("Main programer: Tomas Aquistapace\nMain artist: Joaquin\n\nMusic:\n Halloween Fun - by pinkzebra\n We all gonna die - by Loyalty_Freak_Music", fontSubTittle) / 2), 300, fontSubTittle, WHITE);
+			DrawText("Main programer: Tomas Aquistapace\nMain artist: Joaquin Gonzalez\n\nMusic:\n Halloween Fun - by pinkzebra\n We all gonna die - by Loyalty_Freak_Music", (GetScreenWidth() / 2 - 175) + (350 / 2) - (MeasureText("Main programer: Tomas Aquistapace\nMain artist: Joaquin\n\nMusic:\n Halloween Fun - by pinkzebra\n We all gonna die - by Loyalty_Freak_Music", fontSubTittle) / 2), 300, fontSubTittle, WHITE);
 
 			DrawTexture(Textures::menuArrows, static_cast<int>(Menu::leftArrow.rec.x), static_cast<int>(Menu::leftArrow.rec.y), GRAY);
 			
